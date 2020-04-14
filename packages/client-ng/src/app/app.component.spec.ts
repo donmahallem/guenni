6107634e0ab79875130a0b0e20fcabac9a5b71f1
DonMahallem <donmahallem@users.noreply.github.com>
2020-04-14 14:44:42 +0200

    style(client-ng): fix lint issues related to typedef rule

--- packages/client-ng/src/app/app.component.spec.ts
+++ packages/client-ng/src/app/app.component.spec.ts
@@ -6,8 +6,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+describe('AppComponent', (): void => {
+  beforeEach(async((): void => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
@@ -18,7 +18,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app', (): void => {
     const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
     const app: AppComponent = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
