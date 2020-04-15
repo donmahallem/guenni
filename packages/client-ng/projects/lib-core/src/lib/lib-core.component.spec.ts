8e33a20d213e21b0f35f99b331136c69cd19ebff
donmahallem <donmahallem@users.noreply.github.com>
2020-04-15 09:54:54 +0200

    feat(client-ng): add lib-core
    add lib-core library to client-ng

---
+++ packages/client-ng/projects/lib-core/src/lib/lib-core.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LibCoreComponent } from './lib-core.component';
+
+describe('LibCoreComponent', () => {
+  let component: LibCoreComponent;
+  let fixture: ComponentFixture<LibCoreComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LibCoreComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LibCoreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ packages/client-ng/projects/lib-core/src/lib/lib-core.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-lib-core',
+  template: `
+    <p>
+      lib-core works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LibCoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
