515b329d98f60c28cbb1af46eba593d3de52f81c
donmahallem <donmahallem@users.noreply.github.com>
2020-04-19 00:11:20 +0200

    J

---
+++ packages/client-ng/projects/lib-responsive-img/src/lib/responsive-img.module.ts
@@ -0,0 +1,14 @@
+/*!
+ * Source https://github.com/donmahallem/guenni Package: client-ng
+ */
+
+import { NgModule } from '@angular/core';
+import { ResponsiveImgDirective } from './responsive-img.directive';
+
+@NgModule({
+  declarations: [ResponsiveImgDirective],
+  exports: [ResponsiveImgDirective],
+  imports: [
+  ],
+})
+export class LibResponsiveImgModule { }
