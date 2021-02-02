d899488c1d24e484c7730bce4a786d00d751b567
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-02 09:03:13 +0100

    fix(client-ng): template path

--- packages/client-ng/src/app/app.component.ts
+++ packages/client-ng/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
-  templateUrl: './app.component.pug',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   title: string = 'guenni';
